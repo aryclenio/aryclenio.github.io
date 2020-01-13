710d060a69c9d14800d6b9bbb8b4fa8f0a89a330
Aryclenio Xavier Barros <aryclenio2010@gmail.com>
2020-01-13 12:33:09 -0300

    Add files via upload

---
+++ precache-manifest.8f82d7d2849bf3d274975d2d1d70d9eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e100fe3b0132350b878",
+    "url": "aryclenio.github.io/static/js/runtime~main.6e100fe3.js"
+  },
+  {
+    "revision": "c98e4abff88c2407bf9e",
+    "url": "aryclenio.github.io/static/js/main.c98e4abf.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "aryclenio.github.io/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "c98e4abff88c2407bf9e",
+    "url": "aryclenio.github.io/static/css/main.5e047c97.chunk.css"
+  },
+  {
+    "revision": "a967f08f97cbfd09fe9de3569f430f2a",
+    "url": "aryclenio.github.io/index.html"
+  }
+];
