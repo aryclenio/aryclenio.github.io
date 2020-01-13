68aa78487fa4bcbf88efd5920e27f9fa16ac40eb
Ary <aryclenio2010@gmail.com>
2020-01-13 13:52:54 -0300

    :boom: Updating source

---
+++ src/resumeData.js
@@ -0,0 +1,134 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Aryclenio Barros",
+    "role": "Fullstack Developer",
+    "linkedinId":"arycleniox@ufrn.edu.br",
+    "skypeid": "Your skypeid",
+    "roleDescription": "22 years old and a passion with music and video games. Ready to code at any time...",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/aryclenio-barros-060322135",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/aryclenio",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com/aryxb",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":"Currently student in bachelor degree in Computer Engineering and always looking for new technologies to satisfy my passion that is development. In the future I intend to move to the field of education, I believe that knowledge must always be transmitted and carried forward.",
+    "address":"Natal-RN Brazil",
+    "website":"https://aryclenio.github.io",
+    "education":[
+      {
+        "UniversityName":"Universidade Federal do Rio Grande do Norte",
+        "specialization":"Computer Engeneering",
+        "MonthOfPassing":"Today",
+        "YearOfPassing":"",
+      },
+      {
+        "UniversityName":"Universidade Federal do Rio Grande do Norte",
+        "specialization":"Science and Tecnology",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2019",
+      },
+      {
+        "UniversityName":"Instituto Federal de Educação Ciências e Tenologia do RN",
+        "specialization":"Computer Technician",
+        "MonthOfPassing":"Nov",
+        "YearOfPassing":"2016",
+      },
+      {
+        "UniversityName":"Instituto Metrópole Digital",
+        "specialization":"Web Development Technician",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2014",
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Superintendência de Informática",
+        "specialization":"Front-End Developer",
+        "MonthOfLeaving":"Today",
+        "YearOfLeaving":"",
+      },
+    ],
+    "skillsDescription":"Check my level of experience",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"JQuery"
+      },
+      {
+        "skillname":"ReactJS"
+      },
+      {
+        "skillname":"ReactNative"
+      },
+      {
+        "skillname":"Vuejs"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Repository Finder",
+        "description":"Repository Finder is an SPA made in ReactJS for searching repositories under github.",
+        "imgurl":"images/portfolio/ary.png",
+        "repo":"https://github.com/aryclenio/Repository-Finder"
+      },
+      {
+        "name":"Food Software",
+        "description":"A web system to provide management of a food business company. It was made using the Pug.js and Sass precompilers and includes a Node.js REST API combined with SQL 5.8",
+        "imgurl":"images/portfolio/food.png",
+        "repo":"https://github.com/aryclenio/FoodSoftware"
+      },
+      {
+        "name":"Minesweeper",
+        "description":"A React Native game of Minesweeper. The game has difficulty level and includes all the known features.",  
+        "imgurl":"images/portfolio/campo.png",
+        "repo":"https://github.com/aryclenio/Minesweeper"
+      },
+      {
+        "name":"Sculptor",
+        "description":"A sculptor software build in C++ that draws geometric figures in individual voxels (3d Pixels) and imports the figure to a .off or .vect format, readeable by geomview or meshlab.",
+        "imgurl":"images/portfolio/sculptor.jpg",
+        "repo":"https://github.com/aryclenio/Sculptor"
+      },
+      // {
+      //   "name":"IOS Calculator",
+      //   "description":"A React Native app that mimic the IOS calculator. It makes sum, subtraction, multiplication and division.",  
+      //   "imgurl":"images/portfolio/ios.jpeg"
+      // },
+      // {
+      //   "name":"4-bit Calculator",
+      //   "description":"A bit calculator, implemented in VDHL that provides 4 bits sum, subtraction, inversion, major and minor verification. The result is shown in a 7 segment display on a FPGA board. This code was tested in Altera Quartus II.",  
+      //   "imgurl":"images/portfolio/fpga.jpg"
+      // },
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
